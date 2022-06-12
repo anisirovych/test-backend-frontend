57e031830eeb7630180462a8f815cee20708faa7
anisirovych <serzj1337@gmail.com>
2022-06-12 19:20:15 +0300

    create books router / books adding to DB

--- server.js
+++ server.js
@@ -11,21 +11,27 @@ const mongoose = require('mongoose')
 // Routers
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 const app = express()
 
 
+// View
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 
+// 
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
+// 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
+// Mongo connection
 mongoose.connect(process.env.DB_URL, {
 	useNewUrlParser: true
 })
